8dade2405bb060c0aab0dc849891a92d2059fdc7
Artem Golovin <artgolovin96@gmail.com>
2020-02-04 14:10:19 -0700

    :sparkles: simplify the string builder

--- src/include/string_builder.h
+++ src/include/string_builder.h
@@ -8,30 +8,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define SB_MAX_FRAG_LENGTH 4096
-#define SB_FAILURE -1
+#define SB_STR_FRAG_LENGTH 32
 
-static const size_t STRING_MIN_SIZE = 32;
-
-struct string_fragment_t {
-  struct string_fragment_t *next;
-  int length;
+typedef struct {
   char *str;
-};
-
-typedef struct string_fragment_t string_fragment_t;
-
-struct string_builder_t {
-  string_fragment_t *root;
-  string_fragment_t *trunk;
   int length;
-};
+  int __allocated;
 
-typedef struct string_builder_t string_builder_t;
+} string_builder_t;
 
-/**
- *
- */
 string_builder_t *sb_init();
 
 bool sb_is_empty(string_builder_t *sb);
@@ -40,8 +25,6 @@ bool sb_append(string_builder_t *sb, const char *str);
 
 bool sb_append_char(string_builder_t *sb, char ch);
 
-int sb_appendf(string_builder_t *sb, const char *fmt, ...);
-
 char *sb_build_str(string_builder_t *sb);
 
 void sb_reset(string_builder_t *sb);
@@ -49,3 +32,4 @@ void sb_reset(string_builder_t *sb);
 void sb_free(string_builder_t *sb);
 
 #endif /* STRING_BUILDER_H */
+
--- src/string_builder.c
+++ src/string_builder.c
@@ -2,81 +2,49 @@
 
 string_builder_t *sb_init() {
   string_builder_t *sb =
-      (string_builder_t *)calloc(sizeof(string_builder_t), 1);
+      (string_builder_t *)calloc(1, sizeof(string_builder_t));
+
+  sb->length = 0;
+  sb->str = (char *)malloc(sizeof(char) * SB_STR_FRAG_LENGTH);
+  sb->__allocated = sizeof(char) * SB_STR_FRAG_LENGTH;
+  *sb->str = '\0';
+
   return sb;
 }
 
-bool sb_is_empty(string_builder_t *sb) { return (sb->root == NULL); }
+bool sb_is_empty(string_builder_t *sb) {
+  return sb->length == 0 || sb->str == NULL;
+}
 
 bool sb_append(string_builder_t *sb, const char *str) {
-  int length = strlen(str);
-  string_fragment_t *fragment = (string_fragment_t *)malloc(
-      sizeof(string_fragment_t) + sizeof(char) * length);
-
-  if (fragment == NULL) {
+  if (str == NULL) {
     return false;
   }
 
-  fragment->next = NULL;
-  fragment->length = length;
+  int len = strlen(str);
 
-  // copy str to fragment->str
-  memcpy((void *)&fragment->str, (const void *)str,
-         sizeof(char) * (length + 1));
-
-  sb->length += length;
-
-  if (sb->root == NULL) {
-    sb->root = fragment;
-  } else {
-    sb->trunk->next = fragment;
+  // Add space for a string, if needed
+  while (sb->__allocated < sb->length + len + 1) {
+    sb->__allocated *= 2;
   }
 
-  sb->trunk = fragment;
+  sb->str = (char *) realloc(sb->str, sb->__allocated);
+  memmove(sb->str + sb->length, str, len);
+  sb->length += len;
+  sb->str[sb->length] = '\0';
 
   return true;
 }
 
-bool sb_append_char(string_builder_t *sb, char ch) {
-  char str[1] = {ch};
-  return sb_append(sb, str);
-}
-
-char *sb_build_str(string_builder_t *sb) {
-  // allocate memory for the string
-  // which will be the size of all strings in string vuilder (sb->length)
-  char *strbuf = (char *)malloc((sb->length + 1) * sizeof(char));
-
-  if (strbuf == NULL) {
-    return NULL;
-  }
-
-  // use temp container that points to strbuf
-  char *container = strbuf;
-  for (string_fragment_t *fragment = sb->root; fragment != NULL;
-       fragment = fragment->next) {
-    // copt fragment to string buffer
-    memcpy(container, &fragment->str, sizeof(char) * fragment->length);
-    container += fragment->length;
-  }
-
-  *container = '\0';
-  return strbuf;
-}
+char *sb_build_str(string_builder_t *sb) { return sb->str; }
 
 void sb_reset(string_builder_t *sb) {
-  string_fragment_t *fragment = sb->root;
-  string_fragment_t *next = NULL;
-
-  while (fragment != NULL) {
-    next = fragment->next;
-    free(fragment);
-    fragment = next;
-  }
-
-  sb->root = NULL;
-  sb->trunk = NULL;
+  sb->str = NULL;
   sb->length = 0;
 }
 
-void sb_free(string_builder_t *sb) { free(sb); }
+void sb_free(string_builder_t *sb) {
+  free(sb->str);
+  free(sb);
+}
+

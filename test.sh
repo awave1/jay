85729377bd19d5af86e75ec4f83d9faea419f6d9
Artem Golovin <artgolovin96@gmail.com>
2020-02-06 07:48:10 -0700

    :sparkles: add pass/fail output for test runner

--- test.sh
+++ test.sh
@@ -3,9 +3,19 @@
 TEST_FILES=$(find "$PWD/test/data" -name "*.t*")
 SCANNER=$PWD/scanner
 
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+RESET=$(tput sgr0)
+
 echo -e "running tests...\n"
 for f in $TEST_FILES; do
   echo "Testing: $f"
   SCANNER -f $f
+  if [ $? -eq 0 ]; then
+    echo "${GREEN}PASS${RESET}"
+  else
+    echo "${RED}FAIL${RESET}"
+  fi
+  echo "------------------------------"
 done
 echo -e "\ndone!"

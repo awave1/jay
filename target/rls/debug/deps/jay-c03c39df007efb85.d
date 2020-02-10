97877e9061b751ca46255e7eb8e7bd0b7562825e
Artem Golovin <artgolovin96@gmail.com>
2020-02-10 16:08:04 -0700

    :tada: rust version of the compiler

---
+++ target/rls/debug/deps/jay-c03c39df007efb85.d
@@ -0,0 +1,5 @@
+/Users/awave/.dev/jay/target/rls/debug/deps/jay-c03c39df007efb85.rmeta: src/main.rs
+
+/Users/awave/.dev/jay/target/rls/debug/deps/jay-c03c39df007efb85.d: src/main.rs
+
+src/main.rs:
---
+++ target/rls/debug/deps/jay-cd4a0baf492320ea.d
@@ -0,0 +1,5 @@
+/Users/awave/.dev/jay/target/rls/debug/deps/jay-cd4a0baf492320ea.rmeta: src/main.rs
+
+/Users/awave/.dev/jay/target/rls/debug/deps/jay-cd4a0baf492320ea.d: src/main.rs
+
+src/main.rs:

fd2f06ebbc2a28b22763906b6a26ad43a8c8e913
Artem Golovin <artgolovin96@gmail.com>
2020-04-06 01:40:20 -0600

    :sparkles: add simple codegen for strings

--- src/StringTable.cpp
+++ src/StringTable.cpp
@@ -1,3 +1,20 @@
 #include "StringTable.hpp"
 
 void StringTable::define(std::string str) {}
+
+/**
+ * @brief Generate WASM code with all the strings in the source code
+ *
+ * @return std::string generated WASM code
+ */
+std::string build_wasm_code() {
+    std::string code = "";
+    for (auto const &[str, str_entry] : table) {
+        // (data 0 (i32.const $offset) "str")
+        code += "(data 0 (i32.const " + std::to_string(str_entry.offset) + ") \"" + str +"\")";
+        code += "\n";
+    }
+    
+    return code;
+}
+

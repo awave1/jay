fafbe82e2e121e4a55ea7a1eb2aea5e5c9397baa
awave1 <artgolovin96@gmail.com>
2020-04-17 13:50:50 -0600

    ✅ Test: add test runner

--- test_exec.sh
+++ test_exec.sh
@@ -5,10 +5,13 @@
 TEST_FILES=$(find "$PWD/test/$1" -name "*.*")
 COMPILER=$PWD/jay
 
+make build
+
 echo -e "running tests...\n"
 for f in $TEST_FILES; do
-  # cat $f
-  $COMPILER $f
+  out_name="out_$(basename $f)"
+  $COMPILER $f > "$out_name.wat" && wat2wasm $out_name && wasm-interp --411 "$out_name.wasm"
+
   echo -e "------------------------------------\n"
 done
 echo "done!"
